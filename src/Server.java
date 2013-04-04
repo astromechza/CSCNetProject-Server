75a2a552475ff0e598f05bd20675442559de295a
Mordechai Serraf <mserraf34@gmail.com>
2013-04-04 16:26:58 +0200

    Added methods to be used/requested by clients to server

--- src/Server.java
+++ src/Server.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.json.*;// import all, not sure what exactly is needed for what
 
 import log.Log;
 
@@ -41,6 +42,35 @@ public class Server {
 				e.printStackTrace();
 			}			
 		}
+	}
+	
+	/*
+	 * Methods called by client: 
+	 */
+	
+	public JSONObject update ()
+	{
+		JSONObject jo =  new JSONObject ();
+		jo.put("Result", "update called");
+		return jo;
+	}
+	
+	public JSONObject ping ()
+	{
+		JSONObject jo =  new JSONObject ();
+		jo.put("Result", "ping called");
+		jo.put("Error", "no errors now");
+		jo.put("ID", "ID goes here");
+		return jo;
+	}
+	
+	public JSONObject hello ()
+	{
+		JSONObject jo =  new JSONObject ();
+		jo.put("Result", "hello called");
+		jo.put("Error", "no errors now");
+		jo.put("ID", "ID goes here");
+		return jo;
 	}
 	
 }
